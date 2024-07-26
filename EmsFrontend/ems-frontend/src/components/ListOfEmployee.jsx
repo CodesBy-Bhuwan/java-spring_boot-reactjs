22989b1a5c3ac544bc1e725466731d8959b21cfb
CodesBy-Bhuwan <bhuwan.thadarai817@gamil.com>
2024-07-27 00:07:43 +0545

    Tix-1 ListOfEmployee

---
+++ EmsFrontend/ems-frontend/src/components/ListOfEmployee.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+
+const ListOfEmployee = () => {
+
+    const dummyDbms = [
+        {
+            "id":1,
+            "firstName":"Ram",
+            "lastName":"Mar",
+            "address":"Kathmandu",
+            "email":"ram@gmail.com"
+        },
+        {
+            "id":2,
+            "firstName":"Ram2",
+            "lastName":"Mar2",
+            "address":"Kathmandu2",
+            "email":"ram2@gmail.com"
+        },
+        {
+            "id":3,
+            "firstName":"Ram3",
+            "lastName":"Mar3",
+            "address":"Kathmandu",
+            "email":"ram3@gmail.com"
+        },
+        {
+            "id":4,
+            "firstName":"Ram4",
+            "lastName":"Mar4",
+            "address":"Kathmandu",
+            "email":"ram4@gmail.com"
+        },
+        {
+            "id":5,
+            "firstName":"Ram5",
+            "lastName":"Mar5",
+            "address":"Kathmandu",
+            "email":"ram5@gmail.com"
+        },
+        {
+            "id":6,
+            "firstName":"Ram6",
+            "lastName":"Mar6",
+            "address":"Kathmandu",
+            "email":"ram6@gmail.com"
+        },
+        {
+            "id":7,
+            "firstName":"Ram7",
+            "lastName":"Mar7",
+            "address":"Kathmandu",
+            "email":"ram7@gmail.com"
+        },
+        {
+            "id":8,
+            "firstName":"Ram8",
+            "lastName":"Ma8r",
+            "address":"Kathmandu",
+            "email":"ram8@gmail.com"
+        },
+        {
+            "id":9,
+            "firstName":"Ram9",
+            "lastName":"Mar9",
+            "address":"Kathmandu",
+            "email":"r9am@gmail.com"
+        },
+    ]
+
+  return (
+    <div>
+    <h2>List of Employees</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Employee Id</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Address</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            {
+                dummyDbms.map(employee =>
+                <tr key={employee.id}>
+                    <td>{employee.id}</td>
+                    <td>{employee.firstName}</td>
+                    <td>{employee.lastName}</td>
+                    <td>{employee.address}</td>
+                    <td>{employee.email}</td>
+
+                </tr>
+                
+                )
+            }
+        
+        </tbody>
+    </table>
+
+    </div>
+  )
+}
+
+export default ListOfEmployee
