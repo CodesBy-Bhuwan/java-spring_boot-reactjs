602e6fbf08a5ca9c3d0ca3b73dd39d0ca6600e0a
CodesBy-Bhuwan <bhuwan.thadarai817@gamil.com>
2024-08-02 23:19:21 +0545

    Tix-3.2.2 Footer Component

--- EmsFrontend/ems-frontend/src/components/FooterComponent.jsx
+++ EmsFrontend/ems-frontend/src/components/FooterComponent.jsx
@@ -4,6 +4,7 @@ const FooterComponent = () => {
   return (
     <div>
         <footer className='footer'></footer>
+        <span>All right reserved! 2023 by SomeOne <b>Me.com</b></span>
 
     </div>
   )
