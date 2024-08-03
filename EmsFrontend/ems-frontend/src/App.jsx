394ad0820dca7a430f98e78708bc25ad6bdc3af1
CodesBy-Bhuwan <bhuwan.thadarai817@gamil.com>
2024-08-03 23:48:40 +0545

    Tix-1.2 ListOfEmployee

--- EmsFrontend/ems-frontend/src/App.jsx
+++ EmsFrontend/ems-frontend/src/App.jsx
@@ -3,15 +3,25 @@ import "./App.css";
 import ListOfEmployee from "./components/ListOfEmployee";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+    <BrowserRouter>
       <HeaderComponent />
-      <ListOfEmployee />
+      <Routes>
+        {/* This is when we click on link for like: http://localhost:3000 */}
+        <Route path="/" element={ <ListOfEmployee />} >
+        {/* Similarly for other paths we do the same difference is assign the path */}
+        <Route path="/employees" element {<ListOfEmployee />}></Route>
+
+        </Route>
+      </Routes>
       <FooterComponent />
+      </BrowserRouter>
     </>
   );
 }
